c4258ad105c2185ded315bf252ef75fad8efe1e2
Artem Zyrianov <mister.zyrianovartyom@yandex.ru>
2021-08-11 03:26:53 +0600

    Начал верстку и подключил шрифты

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './Footer.css';
+
+function Footer() {
+	return (
+		<footer className="footer">
+            <div className="footer__content">
+			<p className="footer__description">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+            <div className="footer__menu">
+            <p className="footer__copyright">&copy; 2021</p>
+            <nav className="footer__social">
+                <a href="/#" className="footer__link link">Яндекс.Практикум</a>
+                <a href="/#" className="footer__link link">Github</a>
+                <a href="/#" className="footer__link link">ВКонтакте</a>
+            </nav>
+            </div>
+            </div>
+        </footer>
+	);
+}
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+  
+import React from "react";
+import logo from "../../images/logo.svg";
+import account_icon from "../../images/account-img.svg";
+import { Link } from "react-router-dom";
+import './Header.css';
+
+function Header({userData, onSignOut}) {
+    return (
+        <header className="header">
+            <img className="header__logo" src={logo} alt="Лого" />
+            <nav className="header__menu">
+                    <Link to="/movies" className="header__link link">Фильмы</Link>
+                    <Link to="/saved-movies" className="header__link link">Сохраненные фильмы</Link>
+                    <Link to="/profile" className="header__link header__link_account link">
+                    Аккаунт
+                    <img src={account_icon} alt="иконка" className="header__link menu__account-icon" />
+                    </Link>
+            </nav>
+        </header>
+    );
+}
+export default Header;
