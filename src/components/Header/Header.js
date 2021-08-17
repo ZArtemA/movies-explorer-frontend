512b4a307c6fc6adf69e127618aa6585b9cd8b2f
Artem Zyrianov <mister.zyrianovartyom@yandex.ru>
2021-08-18 04:59:22 +0600

    Адаптировал страницу карточек

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
 import logo from "../../logo.svg";
+import menu_icon from "../../images/menu-btn.svg";
 import account_icon from "../../images/account-img.svg";
 
 import './Header.css';
@@ -19,6 +20,7 @@ function Header({userData, onSignOut}) {
                     <img src={account_icon} alt="иконка" className="header__link header__link_account-icon" />
                     </Link>
             </nav>
+            <img className="header__menu-btn" src={menu_icon} alt="Открыть" />
             </Route>
         </header>
     );
